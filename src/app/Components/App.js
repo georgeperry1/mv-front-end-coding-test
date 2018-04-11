1b665cae057cccc9fe88ba512ec798dc07784e0a
georgeperry1 <gmperry18@gmail.com>
2018-04-11 14:18:32 +0100

    feat: added base files for StarredInfluencers, SuggestedInfluencers and SortControl

--- src/app/Components/App.js
+++ src/app/Components/App.js
@@ -3,14 +3,17 @@
 import { connect } from 'react-redux';
 import React from 'react';
 
+import './App.css';
+
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="main">
         <h1>Hello</h1>
       </div>
     )
   }
 }
 
-export default connect(null, null)(App);
+// export default connect(null, null)(App);
+export default App;
