b2f998d90cc818f0d2e2faa5fc63289699104360
georgeperry1 <gmperry18@gmail.com>
2018-04-15 21:42:07 +0100

    chore: styled stats for StarredInfluencersListItems

--- src/app/Components/StarredInfluencersListItem/StarredInfluencersListItem.js
+++ src/app/Components/StarredInfluencersListItem/StarredInfluencersListItem.js
@@ -9,12 +9,22 @@ import './StarredInfluencersListItem.css';
 class StarredInfluencersListItem extends Component {
   render() {
     const { influencer } = this.props;
+    const followers = influencer.statistics.followers;
+    const engagement = influencer.statistics.engagement;
     return (
       <div className="starred-list-item bobble">
         <InfluencerImage influencer={influencer}/>
         <div className="starred-name-container">
           <p className="starred-name">{influencer.influencer_full_name}</p>
           <InfluencerInstagram influencer={influencer}/>
+        </div>
+        <div className="starred-followers">
+          <p className="starred-stats">{followers}</p>
+          <p className="starred-label">Followers</p>
+        </div>
+        <div className="starred-engagement">
+          <p className="starred-stats">{engagement}%</p>
+          <p className="starred-label">Engagement</p>
         </div>
       </div>
     )
