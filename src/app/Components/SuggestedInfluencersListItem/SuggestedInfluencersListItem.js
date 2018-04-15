673109860399d5b7c15107335e947c89b7bf5062
georgeperry1 <gmperry18@gmail.com>
2018-04-15 16:58:26 +0100

    refactor: SuggestedInfluencerListItem now using class ... extends ...

--- src/app/Components/SuggestedInfluencersListItem/SuggestedInfluencersListItem.js
+++ src/app/Components/SuggestedInfluencersListItem/SuggestedInfluencersListItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import InfluencerImage from '../InfluencerImage';
@@ -6,18 +6,23 @@ import InfluencerInstagram from '../InfluencerInstagram';
 
 import './SuggestedInfluencersListItem.css';
 
-const SuggestedInfluencersListItem = ({ influencer }) => (
-  <div className="suggested-list-item bobble">
-    <InfluencerImage influencer={influencer}/>
-    <div className="suggested-name-container">
-      <p className="suggested-name">{influencer.influencer_full_name}</p>
-      <InfluencerInstagram influencer={influencer}/>
-    </div>
-    <button className="suggested-add">
-      +
-    </button>
-  </div>
-);
+class SuggestedInfluencersListItem extends Component {
+  render() {
+    const { influencer } = this.props;
+    return (
+      <div className="suggested-list-item bobble">
+        <InfluencerImage influencer={influencer}/>
+        <div className="suggested-name-container">
+          <p className="suggested-name">{influencer.influencer_full_name}</p>
+          <InfluencerInstagram influencer={influencer}/>
+        </div>
+        <button className="suggested-add">
+          +
+        </button>
+      </div>
+    )
+  }
+}
 
 SuggestedInfluencersListItem.propTypes = {
   influencer: PropTypes.object,
